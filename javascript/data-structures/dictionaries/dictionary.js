2b606289fbe23ff10a6cac2d6e9e8c676dec1fdf
Richard Baker <richiebkr@gmail.com>
2019-12-25 01:49:11 -0800

    dictionary DS and example

---
+++ javascript/data-structures/dictionaries/dictionary.js
@@ -0,0 +1,27 @@
+function Dictionary() {
+    this.add = add;
+    this.datastore = new Array();
+    this.find = find;
+    this.remove = remove;
+    this.showAll = showAll;
+}
+
+function add(key, value) {
+    this.datastore[key] = value;
+}
+
+function find(key) {
+    return this.datastore[key];
+}
+
+function remove(key) {
+    delete this.datastore[key];
+}
+
+function showAll() {
+    for(var key of Object.keys(this.datastore)) {
+        console.log(key + " -> " + this.datastore[key]);
+    }
+}
+
+module.exports = Dictionary;
---
+++ javascript/data-structures/dictionaries/phonebook.js
@@ -0,0 +1,9 @@
+const Dictionary = require('./dictionary.js');
+
+var pbook = new Dictionary();
+pbook.add("Mike", "123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+console.log("David's extension: " + pbook.find("David"));
+pbook.remove("David");
+pbook.showAll();
